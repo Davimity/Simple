d75c04eda350d0b5d55c5efe421f49cae59da5d8
David Lubián Muñiz <157855784+Davimity@users.noreply.github.com>
2025-01-26 16:34:26 +0100

    SimpleDataCollections 1.0.0

---
+++ SimpleDataCollections/Trees/LazyTree.cs
@@ -0,0 +1,103 @@
+﻿using System.Text;
+using System.Text.Json;
+
+using File = SimpleFiles.File;
+
+namespace SimpleUtilities.DataStructures {
+    public class LazyTree<T> : Tree<T> {
+        #region Variables
+
+            private bool _isExpanded = false;
+            private string path;
+
+        #endregion
+
+        #region Constructors
+
+            public LazyTree(T value, string path) : base(value) {
+                this.path = path;
+            }
+
+            public LazyTree(T value, byte[] path) : base(value) {
+                this.path = Encoding.UTF8.GetString(path);
+            }
+
+        #endregion
+
+        #region Public methods
+
+            ///<summary> Loads the children of the tree from a file. </summary>
+            ///<param name="encrypted"> Whether the file is encrypted. </param>
+            ///<param name="key"> The key to decrypt the file. </param>
+            ///<param name="ivOrNonce"> The IV or nonce to decrypt the file. </param>
+            ///<param name="salt"> The salt used to decrypt the file. </param>
+            ///<param name="encryptionAlgorithm"> The encryption algorithm used to decrypt the file. </param>
+            public void LoadChildrenFromFile(bool encrypted = false, byte[]? key = null, byte[]? ivOrNonce = null,
+                byte[]? salt = null, File.EncryptionAlgorithm encryptionAlgorithm = File.EncryptionAlgorithm.ChaCha20) {
+                if (_isExpanded) return;
+
+                if (!File.Exists(path)) {
+                    _isExpanded = true;
+                    return;
+                }
+
+                byte[] fileBytes = File.Read(Encoding.UTF8.GetBytes(path), decrypt: encrypted, key: key, ivOrNonce: ivOrNonce, salt: salt, encryptionAlgorithm: encryptionAlgorithm);
+
+                if(fileBytes.Length == 0) return;
+
+                List<Tree<T>>? children = JsonSerializer.Deserialize<List<Tree<T>>>(fileBytes);
+                if(children == null) return;
+
+                foreach(var child in children) AddChild(child);
+                _isExpanded = true;
+            }
+
+            ///<summary> Unloads the children of the tree. </summary>
+            ///<param name="saveBeforeUnload"> Whether the children should be saved before unloading. </param>
+            ///<param name="encrypted"> Whether the children should be saved before unloading. </param>
+            ///<param name="key"> The key to encrypt the children. </param>
+            ///<param name="ivOrNonce"> The IV or nonce to encrypt the children. </param>
+            ///<param name="salt"> The salt used to encrypt the children. </param>
+            ///<param name="encryptionAlgorithm"> The encryption algorithm used to encrypt the children. </param>
+            public void UnloadChildren(bool saveBeforeUnload = true, bool encrypted = false, byte[]? key = null, byte[]? ivOrNonce = null,
+                byte[]? salt = null, File.EncryptionAlgorithm encryptionAlgorithm = File.EncryptionAlgorithm.ChaCha20) {
+                if (!_isExpanded) return;
+
+                if (saveBeforeUnload) SaveChildrenToFile(encrypted, key, ivOrNonce, salt, encryptionAlgorithm);
+
+                RemoveChildren();
+                _isExpanded = false;
+            }
+
+            ///<summary> Saves the children of the tree to a file. </summary>
+            ///<param name="encrypted"> Whether the file should be encrypted. </param>
+            ///<param name="key"> The key to encrypt the file. </param>
+            ///<param name="ivOrNonce"> The IV or nonce to encrypt the file. </param>
+            ///<param name="salt"> The salt used to encrypt the file. </param>
+            ///<param name="encryptionAlgorithm"> The encryption algorithm used to encrypt the file. </param>
+            public void SaveChildrenToFile(bool encrypted = false, byte[]? key = null, byte[]? ivOrNonce = null,
+                byte[]? salt = null, File.EncryptionAlgorithm encryptionAlgorithm = File.EncryptionAlgorithm.ChaCha20) {
+               
+                if(!File.Exists(path)) return;
+
+                List<Tree<T>>[] grandchildren = new List<Tree<T>>[Children.Count];
+
+                for (int i = 0; i < grandchildren.Length; i++) {
+                    grandchildren[i] = new List<Tree<T>>(Children[i].Children);
+                    Children[i].RemoveChildren(false);
+                }
+
+                var children = new List<Tree<T>>(Children);
+                byte[] data = JsonSerializer.SerializeToUtf8Bytes(children);
+
+                File.Write(Encoding.UTF8.GetBytes(path), data, append: false, encrypt: encrypted, key: key, ivOrNonce: ivOrNonce, salt: salt, encryptionAlgorithm: encryptionAlgorithm);
+
+                for (int i = 0; i < grandchildren.Length; i++) {
+                    Children[i].AddChildren(grandchildren[i]);
+                    grandchildren[i].Clear();
+                }
+            }
+
+        #endregion
+    }
+}
